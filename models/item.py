1bd62f1f3574cd8762e5b03cd37da52e6dd755ac
danijuster <danijuster@gmail.com>
2020-10-11 21:58:10 +0300

    First commit.

---
+++ models/item.py
@@ -0,0 +1,53 @@
+import uuid
+from typing import Dict
+import requests
+from bs4 import BeautifulSoup
+from dataclasses import dataclass, field
+import re
+from models.model import Model
+
+
+@dataclass(eq=False)
+class Item(Model):
+    collection: str = field(init=False, default="items")
+    url: str
+    tag_name: str
+    query: Dict
+    price: float = field(default=None)
+    _id: str = field(default_factory=lambda: uuid.uuid4().hex)
+
+    def load_price(self) -> float:
+        headers = {
+            # 'authority': 'www.amazon.com',
+            # 'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'dnt': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        }
+        response = requests.get(self.url, headers=headers)
+        content = response.content
+        soup = BeautifulSoup(content, "html.parser")
+        element = soup.find(self.tag_name, self.query)
+        string_price = element.text.strip()
+
+        pattern = re.compile(r"(\d+,?\d*\.\d\d)")
+        match = pattern.search(string_price)
+        found_price = match.group(1)
+        without_commas = found_price.replace(",", "")
+        self.price = float(without_commas)
+        return self.price
+
+    def json(self) -> Dict:
+        return {
+            "_id": self._id,
+            "url": self.url,
+            "tag_name": self.tag_name,
+            "price": self.price,
+            "query": self.query
+        }
